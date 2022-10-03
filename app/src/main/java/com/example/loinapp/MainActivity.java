9e590b02633c2684130467238af24676602e1aa5
410954556 <s1095455>
2022-10-04 07:12:43 +0800

    new

--- app/src/main/java/com/example/loinapp/MainActivity.java
+++ app/src/main/java/com/example/loinapp/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.loinapp;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -25,15 +27,37 @@ public class MainActivity extends AppCompatActivity {
                 if ((name.getText().toString().equals("james")) &&
                         (pw.getText().toString().equals("pw"))) {
 
-                    Intent it = new Intent(getApplicationContext(),Userctivity.class);
-                    startActivity(it);
+
+                    Bundle data = new Bundle();
+                    data.putString("account", name.getText().toString());
+                    data.putInt("age", 24);
+
+                    Intent it = new Intent(getApplicationContext(), Userctivity.class);
+
+                    it.putExtras(data);
+                    startActivityForResult(it, 100);
 
                     Toast.makeText(getApplicationContext(), "成功", Toast.LENGTH_LONG).show();
 
-                }else{
+                } else {
                     Toast.makeText(getApplicationContext(), "登入失敗", Toast.LENGTH_LONG).show();
                 }
             }
         });
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 100) {
+
+            if (resultCode == 1000) {
+                Bundle da = data.getExtras();
+                String val = da.getString("key");
+                Toast.makeText(getApplicationContext(), val, Toast.LENGTH_LONG).show();
+            }
+        }
     }
 }
--- app/src/main/java/com/example/loinapp/Userctivity.java
+++ app/src/main/java/com/example/loinapp/Userctivity.java
@@ -3,18 +3,34 @@ package com.example.loinapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Debug;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class Userctivity extends AppCompatActivity {
+
+    MediaPlayer mp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_userctivity);
 
+
+        mp=MediaPlayer.create(getApplicationContext(),R.raw.dingdong);
+        mp.start();
+
+        Bundle data = this.getIntent().getExtras();
+        String name=data.getString("account");
+        int age = data.getInt("age");
+
+        Toast.makeText(getApplicationContext(),name+age, Toast.LENGTH_LONG).show();
+       
         Button dialbtn = findViewById(R.id.dialbtn);
         dialbtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -38,8 +54,36 @@ public class Userctivity extends AppCompatActivity {
         exitbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+                Intent it = getIntent();
+                Bundle data = new Bundle();
+                data.putString("key","mydata");
+                it.putExtras(data);
+                setResult(1000,it);
+
                 finish();
             }
         });
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i("log", "onStop");
+        mp.stop();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i("log", "onPause");
+        mp.pause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i("log", "onResume");
+        mp.start();
     }
 }
